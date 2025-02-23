7cb366dd929f3d7c89d22873cb5ba8e5d7e3661e
Potenho <viisael97@hotmail.com>
2025-02-23 07:45:19 -0300

    :sparkles: feat (app & routes): Cleared app component and added editor routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EditorComponent } from './view/editor/editor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'editor', pathMatch: 'full' },
+    { path: 'editor', component: EditorComponent },
+];
