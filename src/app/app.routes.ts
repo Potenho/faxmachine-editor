cd1933bd80d2c9a6e431ea0097d6636ea83a5c88
Potenho <viisael97@hotmail.com>
2025-09-13 12:43:54 -0300

    :sparkles: feat: Implemented front page!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,9 @@ import { Main } from './views/main/main';
 import { isEditingGuard } from './common/guards/is-editing/is-editing-guard';
 import { NewsScript } from './views/editor/sections/news-script/news-script';
 import { Teams } from './views/editor/sections/teams/teams';
-import { isNotEditingGuard } from './common/guards/is-not-editing/is-not-editing-guard';
 
 export const routes: Routes = [
-  { path: '', component: Main, canActivate: [isNotEditingGuard] }, 
+  { path: '', component: Main }, 
   { path: 'editor', component: Editor, canActivateChild: [isEditingGuard], canActivate: [isEditingGuard], children: [
     { path: '', redirectTo: 'news-script', pathMatch: 'full' },
     { path: 'news-script', component: NewsScript },
