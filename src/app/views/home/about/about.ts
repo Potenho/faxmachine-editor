05ffcb220d25a316afe7c781d9467762d212b3c7
Potenho <viisael97@hotmail.com>
2025-09-18 21:49:18 -0300

    :sparkles: feat: about page

--- src/app/views/home/about/about.ts
+++ src/app/views/home/about/about.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.scss'
 })
 export class About {
-
+  githubRepo = environment.githubRepo;
 }
